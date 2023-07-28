5dc6681934ed3cf7b13cc4e39bcb048e7d0710e0
Gabri-ella-1 <ellabella0110@gmail.com>
2023-07-27 20:28:10 -0400

    Client del works
    heck yea

---
+++ PP.MAUI/ViewModels/ClientViewModel.cs
@@ -0,0 +1,81 @@
+﻿using PP.Library.Models;
+using PP.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace PP.MAUI.ViewModels
+{
+    public class ClientViewModel : INotifyPropertyChanged
+
+    {
+        public Client SelectedClient { get; set; }
+
+
+        public ObservableCollection<Client> Clients
+        {
+            get
+            {
+                return new ObservableCollection<Client>(ClientService.Current.Clients);
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /*
+        public string Query { get; set; }
+
+        public void Search()
+        {
+            NotifyPropertyChanged("Clients");
+        }
+        */
+        public void Delete()
+        {
+
+            if (SelectedClient != null)
+            {/*
+                    Console.WriteLine("DELETED");
+                    ClientService.Current.Delete(SelectedClient.Id);
+                    Console.WriteLine(SelectedClient.Id);
+                    NotifyPropertyChanged(nameof(Clients));
+                    //SelectedClient = null;
+                    // NotifyPropertyChanged(nameof(Clients));*/
+                ClientService.Current.Delete(SelectedClient.Id);
+                SelectedClient = null;
+                NotifyPropertyChanged(nameof(Clients));
+                NotifyPropertyChanged(nameof(SelectedClient));
+            }
+
+            // NotifyPropertyChanged(nameof(Clients));
+
+
+        }
+
+        //public Client SelectedClient { get; set; }
+
+
+
+
+
+    }
+
+
+
+
+
+}
--- PP.MAUI/Views/ClientView.xaml.cs
+++ PP.MAUI/Views/ClientView.xaml.cs
@@ -1,9 +1,47 @@
-﻿namespace PP.MAUI.Views;
+﻿using PP.MAUI.ViewModels;
+
+namespace PP.MAUI.Views;
 
 public partial class ClientView : ContentPage
 {
-	public ClientView()
-	{
-		InitializeComponent();
-	}
+    public ClientView()
+    {
+        InitializeComponent();
+        BindingContext = new ClientViewModel();
+    }
+
+    private void SearchClicked(object sender, EventArgs e)
+    {
+        //(BindingContext as ClientViewModel).Search();
+    }
+
+
+
+    private void DeleteClicked(object sender, EventArgs e)
+    {
+        (BindingContext as ClientViewModel).Delete();
+    }
+
+    private void AddClicked(System.Object sender, System.EventArgs e)
+    {
+       // Shell.Current.GoToAsync("//Add");
+    }
+
+    private void EditClicked(System.Object sender, System.EventArgs e)
+    {
+       // Shell.Current.GoToAsync("//Edit");
+    }
+
+    private void ProjectClicked(System.Object sender, System.EventArgs e)
+    {
+        //Shell.Current.GoToAsync("//Project");
+
+    }
+
+    private void GoBackClicked(System.Object sender, System.EventArgs e)
+    {
+       // Shell.Current.GoToAsync("//MainPage");
+
+    }
+
 }
